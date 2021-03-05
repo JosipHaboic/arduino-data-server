7d8d87dbd0165928dfb23c80ded62fe460bf8d16
jhaboic <josiphaboic@gmail.com>
2021-03-05 16:36:01 +0100

    add space

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { resolvers } from './graphql/resolvers';
 import { typeDefs } from './graphql/types';
 
 
-const server = new ApolloServer({ typeDefs, resolvers});
+const server = new ApolloServer({ typeDefs, resolvers });
 server.listen(3000).then(({ url }) => {
 	console.log(`Running GraphQL API Server on ${url}`);
 });
